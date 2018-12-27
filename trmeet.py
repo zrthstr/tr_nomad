7cc221bd18a399da9fdecd71a34e36c476794a06
zrthstr <zrth1k@gmail.com>
2018-12-27 15:08:33 +0100

    post working

--- trmeet.py
+++ trmeet.py
@@ -4,9 +4,11 @@ __version__='0.0.1'
 __author__='zrth1k@gmail.com'
 
 import sys
+import geocoder
 import requests
 import argparse
 import logging
+import datetime
 import configparser
 
 def logger():
@@ -35,6 +37,7 @@ def parse_config():
     post_sufix = '/api/offers'
     auth_sufix = '/api/auth/signin'
     list_sufix = '/api/offers-by/{}?types=meet'
+    delete_sufix = '/api/offers/{}'
 
     config = configparser.ConfigParser()
     config.read(ini_file)
@@ -43,6 +46,7 @@ def parse_config():
     global login_url
     global list_url
     global post_url
+    global delete_url
 
     global login_data
     global post_data
@@ -58,6 +62,7 @@ def parse_config():
     login_url = base_url + auth_sufix
     list_url = base_url + list_sufix
     post_url = base_url + post_sufix
+    delete_url = base_url + delete_sufix
 
     return c
 
@@ -96,6 +101,8 @@ def list_meets(s, tr_id):
     meets = s.get(list_url)
     print(meets.text)
     print(meets.status_code)
+    print("JJJJJJJ",meets.json())
+    return meet_ids
 
 
 def test_auth(tr_id):
@@ -103,11 +110,38 @@ def test_auth(tr_id):
     sys.exit()
 
 
-def clear_meets(s, tr_id):
-    pass
+def clear_meets(s, meet_ids):
+    for mid in meet_ids:
+        log.warning("removing: {}".format(mid))
+        url =  meets_url.format(mid)
+        s.delete(url)
+
 
 def post_new(s):
-    
+    print("POST NEW")
+    headers = {'Content-Type':'application/json;charset=UTF-8',
+                'Accept':'application/json, text/plain, */*'}
+    latlng = get_latlng()
+    #end_time = datetime.datetime.utcnow().isoformat() + 'Z'
+    ## 2 days hard coded for now
+    end_time = (datetime.datetime.now() + datetime.timedelta(days=2)).isoformat() + "Z"
+    log.warning("Posting new meet: {}@{}".format(latlng, end_time))
+    post_data['location'] = latlng
+    post_data['validUntil'] = end_time
+    log.warning("post_data: " +  str(post_data))
+    print("post_url:", post_url)
+    import json
+    meet = s.post(post_url ,data=json.dumps(post_data), headers=headers)
+    log.warning(meet.status_code)
+    log.warning(meet.text)
+
+
+def get_latlng():
+    g = geocoder.ip('me') 
+    latlng = g.latlng
+    print("Found location:", latlng)
+    return latlng
+
 
 def main():
     global log
